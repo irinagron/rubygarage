bed93011bcee918620b53db84541c9b2823ce779
Irina Gron <irinagron@gmail.com>
2013-07-22 23:08:51 +0300

    my mega message

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,24 +1,32 @@
 $(document).ready(function(){
     $(document).on('submit', '.add_task form', function(event){
         event.preventDefault();
+        /*
+         @todo ajax query for saving a task to the db
+         */
+        var that = this;
+
+        console.log($(this).find("[name=name]"));
+        console.log($(that).find("[name=name]"));
         /**
          * @todo ajax query for saving a task to the db
          */
-        $(this).parents('.todo').children('list_of_tasks').append(
+        console.log($(this).parents('.todo').children('.list_of_tasks ul'));
+        $(this).parents('.todo').find('.list').append(
             '<li class="task">       ' +
                 '         <div class="row">      ' +
                 '      <div class="span1 done">' +
                 '                <input type="checkbox"/>' +
                 '            </div>' +
                 '<div class="span9 name">' +
-                $(this).find('[name=name]').val() +
+                $(that).find('[name=name]').val() +
                 '        </div>' +
                 '        <div class="span2 actions">' +
                 '            <div class="btn-toolbar">' +
                 '                <div class="btn-group-primary">' +
                 '                    <a class="btn" href="javascript:void(0)"><i class="icon-move"></i></a>' +
                 '                    <a class="btn" href="javascript:void(0)"><i class="icon-edit"></i></a>' +
-                '                    <a class="btn" href="javascript:void(0)"><i class="icon-remove"></i></a>' +
+                '                    <a class="btn remove_task" href="javascript:void(0)"><i class="icon-remove"></i></a>' +
                 '                </div>' +
                 '            </div>' +
                 '        </div>' +
@@ -26,4 +34,9 @@ $(document).ready(function(){
                 '        </li>'
         )
     })
+    $(document).on('click', '.remove_task', function(){
+        $(this).parents('.task').fadeOut(function(){
+            $(this).remove();
+        });
+    });
 })
